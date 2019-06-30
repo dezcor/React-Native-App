9014488f683cdced301bd7d658656a810562ea92
dezcorjm <dezcorjm@gmail.com>
2019-06-29 19:13:53 -0500

    Primera modificacion

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text,TextInput, View } from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View style={{padding: 40}}>
+        <TextInput
+          style={{height: 40, fontSize: 42}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
